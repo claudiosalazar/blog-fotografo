3dea09a1113b979239f46292deebb27e974509f8
Claudio Salazar <contacto@claudiosalazar.cl>
2025-01-03 03:46:37 -0300

    Camrio a config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   distDir: 'prod',
   output: 'export',
+  env: {
+    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  },
 };
 
 export default nextConfig;
